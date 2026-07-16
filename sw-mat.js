7b1ee0cdf68a512d8f6a872b83147da19e63ccc2
Abdul-Latif Ahmed <alatipha@ymail.com>
2026-07-16 14:01:21 +0000

    Update app version to v3.0.6

--- sw-mat.js
+++ sw-mat.js
@@ -7,7 +7,7 @@
    get a "New version available" prompt on next visit.
 ===================================================== */
 
-const APP_VERSION  = "v3.0.5";
+const APP_VERSION  = "v3.0.6";
 const CACHE_NAME   = `alatipha-ges-mat1-${APP_VERSION}`;
 
 const FILES_TO_CACHE = [
